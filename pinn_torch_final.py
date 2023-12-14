1072ed806c35c2bc0ac1bdeb1d5cb50d3e7db488
Reza Salatin <salatin.r@gmail.com>
2023-12-14 09:23:05 -0500

    Update pinn_torch_final.py

--- pinn_torch_final.py
+++ pinn_torch_final.py
@@ -109,8 +109,8 @@ class PhysicsInformedNN():
             max_iter=50000, 
             max_eval=62500, 
             history_size=100,
-            tolerance_grad=1e-7, 
-            tolerance_change=1e-9,
+            tolerance_grad=1e-5, 
+            tolerance_change=1e-7,
             line_search_fn="strong_wolfe"
         )
 
@@ -124,9 +124,9 @@ class PhysicsInformedNN():
     def net_u(self, t, x, y):  
 
         # input normalization between -1 and 1
-        t = 2.0 * (t - self.vals_min[0])/(self.vals_max[0]-self.vals_min[0]) - 1.0
-        x = 2.0 * (x - self.vals_min[1])/(self.vals_max[1]-self.vals_min[1]) - 1.0
-        y = y #2.0 * (y - self.vals_min[2])/(self.vals_max[2]-self.vals_min[2]) - 1.0
+        #t = 2.0 * (t - self.vals_min[0])/(self.vals_max[0]-self.vals_min[0]) - 1.0
+        #x = 2.0 * (x - self.vals_min[1])/(self.vals_max[1]-self.vals_min[1]) - 1.0
+        #y = y #2.0 * (y - self.vals_min[2])/(self.vals_max[2]-self.vals_min[2]) - 1.0
         
         output = self.dnn(torch.cat([t, x, y], dim=1))
         return output
