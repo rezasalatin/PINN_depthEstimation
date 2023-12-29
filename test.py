c178b0bf4339326e19d9303563215c74c64d4aa0
Reza Salatin <salatin.r@gmail.com>
2023-12-29 12:42:44 -0500

    Update test.py

--- test.py
+++ test.py
@@ -48,16 +48,20 @@ class pinn:
             line_search_fn=self.config['lbfgs_optimizer']['line_search_fn']
         )
 
-    def test(self, test_input_data):
+    def test(self, test_input_data, file_no):
         test_input_data = torch.tensor(test_input_data).float().to(self.device)
         
         # temporal and spatial information for physics part
         for i, (var_name, var_info) in enumerate(self.test_input_vars.items()):
-            requires_grad = "true" in var_info["requires_grad"]
-            setattr(self, var_name, torch.tensor(test_input_data[:, i:i+1], requires_grad=requires_grad).float().to(self.device))  
+            tensor = test_input_data[:, i:i+1].clone().detach()
+            if "true" in var_info["requires_grad"]:
+                tensor = tensor.requires_grad_()
+            tensor = tensor.float().to(self.device)
+            setattr(self, var_name, tensor)
+            
         test_input_data = [getattr(self, var_name) for i, var_name in enumerate(self.test_input_vars)]
 
-        test_prediction_data = self.model(test_input_data)
+        test_prediction_data = self.model(torch.cat(test_input_data, dim=-1))
 
         # Check if optimization is to be performed
         if self.config.get('perform_optimization', False):
@@ -75,9 +79,11 @@ class pinn:
             self.optimizer_LBFGS.step(closure)
 
             with torch.no_grad():
-                test_prediction_data = self.model(torch.cat(tensors, dim=-1))
+                test_prediction_data = self.model(torch.cat(test_input_data, dim=-1))
 
-        test_prediction_data = test_prediction_data.cpu().numpy()
+        test_prediction_data = test_prediction_data.detach().cpu().numpy()
+        
+        print(f'Test data {file_no}')
 
         return test_prediction_data
 
@@ -97,7 +103,7 @@ if __name__ == "__main__":
     y_min, y_max = config['numerical_model']['y_min'], config['numerical_model']['y_max']
     dt = config['numerical_model']['dt']
 
-    file_no = config['numerical_model']['number_of_files']
+    num_files = config['numerical_model']['number_of_files']
 
     tester = pinn(model_path, config)
 
@@ -105,9 +111,9 @@ if __name__ == "__main__":
     y = np.linspace(y_min, y_max, num=config['numerical_model']['ny']).astype(np.float64)
     X_test, Y_test = np.meshgrid(x, y)
 
-    for i in range(file_no):
+    for file_no in range(num_files):
         
-        file_suffix = str(i).zfill(5)
+        file_suffix = str(file_no).zfill(5)
 
         # Dictionary to store the loaded data
         test_input_data = {}
@@ -121,7 +127,7 @@ if __name__ == "__main__":
             elif key == 'y':
                 data = Y_test
             elif key == 't':
-                data = np.full(X_test.shape, i*dt, dtype=np.float64)
+                data = np.full(X_test.shape, file_no*dt, dtype=np.float64)
             else:
                 fname = file_name if key == 'h' else f"{file_name}_{file_suffix}"    
                 file_path = os.path.join(folder, fname)
@@ -131,4 +137,4 @@ if __name__ == "__main__":
             
         test_input_data = np.column_stack([test_input_data[key].flatten() for key in config['data_residual']['inputs']])
 
-        test_outputs = tester.test(test_input_data)
+        test_outputs = tester.test(test_input_data, file_no)
